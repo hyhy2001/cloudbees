734f5ce379d70afa3928a202508b60f48a28027d
Huy Nguyen <huy210701@gmail.com>
2026-04-06 07:54:18 +0000

    Update

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ setup(
     package_data={"": ["*.sql", "*.tcss"]},
     include_package_data=True,
     install_requires=[
-        "click>=8.1",
-        "httpx>=0.27",
-        "cryptography>=42.0",
-        "textual>=0.85.0",
+        "click",
+        "httpx",
+        "cryptography",
+        "textual",
         "rich",
     ],
     entry_points={
